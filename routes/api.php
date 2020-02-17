0b45e317b0a9197d34ab8a1cff57f9967e6d23e3
Lizeth Vargas <lizethvargas@MacBook-Air-de-Lizeth-2.local>
2020-02-16 23:39:23 -0600

    Falta de cors

--- routes/api.php
+++ routes/api.php
@@ -18,60 +18,22 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 
+Route::resource('c_roles', 'C_roleAPIController')->middleware('auth:api');
 
+Route::resource('c_horarios', 'C_horarioAPIController')->middleware('auth:api');
 
+Route::resource('c_giro_negocios', 'C_giro_negocioAPIController')->middleware('auth:api');
 
+Route::resource('t_eventos', 'T_eventoAPIController')->middleware('auth:api');
 
+Route::resource('t_personas', 'T_personaAPIController')->middleware('auth:api');
 
+Route::resource('c_tipo_usuarios', 'C_tipo_usuarioAPIController')->middleware('auth:api');
 
+Route::resource('c_universidads', 'C_universidadAPIController')->middleware('auth:api');
 
+Route::resource('c_tipo_eventos', 'C_tipo_eventoAPIController')->middleware('auth:api');
 
+Route::resource('c_municipios', 'C_municipioAPIController')->middleware('auth:api');
 
-
-Route::resource('c_roles', 'C_roleAPIController');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-Route::resource('c_horarios', 'C_horarioAPIController');
-
-
-
-Route::resource('c_giro_negocios', 'C_giro_negocioAPIController');
-
-
-
-
-
-
-
-
-
-Route::resource('t_eventos', 'T_eventoAPIController');
-
-
-
-Route::resource('t_personas', 'T_personaAPIController');
-
-
-
-Route::resource('c_tipo_usuarios', 'C_tipo_usuarioAPIController');
-
-Route::resource('c_universidads', 'C_universidadAPIController');
-
-Route::resource('c_tipo_eventos', 'C_tipo_eventoAPIController');
-
-Route::resource('c_municipios', 'C_municipioAPIController');
-
-Route::resource('c_estados', 'C_estadoAPIController');
+Route::resource('c_estados', 'C_estadoAPIController')->middleware('auth:api');
