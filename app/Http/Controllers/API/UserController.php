aff4522c10243d3f9d1e3f2f7f77c65358fc3c74
eduardo <eduardo@miedyno.com>
2020-02-22 13:53:54 -0600

    login casi terminado pero funcional

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -30,21 +30,31 @@ public $successStatus = 200;
      */ 
     public function register(Request $request) 
     { 
+        $messages = [
+            'name.required' => 'El nombre es obligatorio',
+            'email.unique' => 'El correo electronico ya esta registrado',
+            'email.required' => 'El correo es obligatorio',
+            'email.email' => 'El formato del correo es incorrecto',
+            'password.required' => 'La contraseña es obligatoria',
+            'password.min' => 'La contraseña debe tener mas de :min caracteres',
+            'c_password.same' => 'Las contraseñas no son identicas',
+            
+        ];
         $validator = Validator::make($request->all(), [ 
             'name' => 'required', 
             'email' => 'required|email|unique:users', 
-            'password' => 'required', 
+            'password' => 'required|min:8', 
             'c_password' => 'required|same:password', 
-        ]);
+        ],$messages);
 if ($validator->fails()) { 
             return response()->json(['error'=>$validator->errors()], 401);            
         }
 $input = $request->all(); 
         $input['password'] = bcrypt($input['password']); 
         $user = User::create($input); 
-        $success['access_token'] =  $user->createToken('MyApp')-> accessToken; 
+        $success['access_token'] =  $user->createToken('MyApp')->accessToken; 
         $success['name'] =  $user->name;
-return response()->json(['success'=>$success], $this-> successStatus); 
+return response()->json(['success'=>$success], 200); 
     }
 /** 
      * details api 
@@ -54,6 +64,6 @@ return response()->json(['success'=>$success], $this-> successStatus);
     public function details() 
     { 
         $user = Auth::user(); 
-        return response()->json(['success' => $user], $this-> successStatus); 
+        return response()->json(['success' => $user], $this->successStatus); 
     } 
 }
